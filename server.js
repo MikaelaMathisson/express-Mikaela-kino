b35165db9eb722cb41f2c3f880bf807791d9c023
Mikaela Mathisson <128062820+MikaelaMathisson@users.noreply.github.com>
2024-01-23 11:40:14 +0100

    adding routes for pages and removing unused filter from previous project

--- server.js
+++ server.js
@@ -14,6 +14,21 @@ app.use(logger)
 app.get('/about', (req, res) => {
     res.sendFile(__dirname + "/public/about.html");
 });
+app.get('/agelimit', (req, res) => {
+    res.sendFile(__dirname + "/public/age-limit.html");
+});
+app.get('/contact', (req, res) => {
+    res.sendFile(__dirname + "/public/contact.html");
+});
+app.get('/movies', (req, res) => {
+    res.sendFile(__dirname + "/public/movies.html");
+});
+app.get('/news', (req, res) => {
+    res.sendFile(__dirname + "/public/news.html");
+});
+app.get('/questions', (req, res) => {
+    res.sendFile(__dirname + "/public/QandA.html");
+});
 
 function logger(req, res, next){
 console.log(req.originalUrl)

770da3abd7f29027fbde22fe4c73746f68a573bc
Mikaela Mathisson <128062820+MikaelaMathisson@users.noreply.github.com>
2024-01-23 11:35:35 +0100

    adding routes for about and removing unnecessary code

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const express = require('express')
+
 const app = express()
 
-app.use(express.static("public"))
+app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
-app.set("view engine", "handlebars")
+
+app.set('view engine', 'handlebars')
+
 app.use(logger)
 
 
-app.get('/', (req, res) => {
-    res.send('Hello');
+app.get('/about', (req, res) => {
+    res.sendFile(__dirname + "/public/about.html");
 });
 
 function logger(req, res, next){

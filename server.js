88e4ffdde2a2f69d73eeceb3826cfc3ddf24f43e
Mikaela Mathisson <128062820+MikaelaMathisson@users.noreply.github.com>
2024-01-23 11:04:02 +0100

    changin back to folder name public

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
 const express = require('express')
 const app = express()
 
-app.use(express.static("static"))
+app.use(express.static("public"))
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
-
-app.set("view engine", "ejs")
+app.set("view engine", "handlebars")
 app.use(logger)
 
 
-app.get('/about', (req, res) => 
-{
-    res.send();
-})
+app.get('/', (req, res) => {
+    res.send('Hello');
+});
 
 function logger(req, res, next){
 console.log(req.originalUrl)
 next()
 }
+
+
 
 app.listen(5080)

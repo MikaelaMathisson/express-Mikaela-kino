0c082c60713043f8111cb979534efe5718c3c92b
Mikaela Mathisson <128062820+MikaelaMathisson@users.noreply.github.com>
2024-01-26 11:43:34 +0100

    adding the last comments

--- server.js
+++ server.js
@@ -3,17 +3,26 @@ import { engine } from "express-handlebars";
 import { loadMovie, loadMovies } from "./src/movies.js";
 export const app = express();
 
+//Serving all the files from kino, making them public to use
+// like for example /index.html will show index
 app.use(express.static("public"));
 
+//Setting engine to handlebars and views to /templates
 app.engine("handlebars", engine());
 app.set("view engine", "handlebars");
 app.set("views", "./templates");
 
+// Get /home from the user,
+// load movies from loadMovies function
+// render home page by showing movie on movie.handlebars
 app.get("/home", async (req, res) => {
   const movies = await loadMovies();
   res.render("home", { movies });
 });
 
+//Get /movies/:movieid from user,
+// Load special movie from api
+// render movie page for specific movie
 app.get("/movies/:movieId", async (req, res) => {
   const movie = await loadMovie(req.params.movieId);
   res.render("movie", { movie });
--- tests/movies.spec.js
+++ tests/movies.spec.js
@@ -2,10 +2,63 @@ import { expect, test } from '@jest/globals';
 import request from 'supertest';
 import app from '../src/app';
 
-test('Encanto page shows title of movie', async () => {
+// Hardcoded tests for the current movies. If added movies to the api
+// there wont be a test for these.
+
+// The test requests the express app, gets the movie with id 8 
+// and checks content type for html
+// then checks if the html matches (toMatch) the title I have written
+test('The Pulp fiction page shows title of movie', async () => {
+    const response = await request(app).get('/movies/8')
+      .expect('Content-Type', 'text/html; charset=utf-8')
+      .expect(200);
+  
+    expect(response.text).toMatch('Pulp Fiction');
+  });
+test('The Encanto page shows title of movie', async () => {
   const response = await request(app).get('/movies/2')
     .expect('Content-Type', 'text/html; charset=utf-8')
     .expect(200);
 
   expect(response.text).toMatch('Encanto');
 });
+
+test('The Isle of dogs page shows title of movie', async () => {
+    const response = await request(app).get('/movies/1')
+      .expect('Content-Type', 'text/html; charset=utf-8')
+      .expect(200);
+  
+    expect(response.text).toMatch('Isle of dogs');
+  });
+
+test('The Min granne totoro page shows title of movie', async () => {
+    const response = await request(app).get('/movies/4')
+      .expect('Content-Type', 'text/html; charset=utf-8')
+      .expect(200);
+  
+    expect(response.text).toMatch('Min granne Totoro');
+  });
+
+test('The Shawshank Redemption page shows title of movie', async () => {
+    const response = await request(app).get('/movies/3')
+      .expect('Content-Type', 'text/html; charset=utf-8')
+      .expect(200);
+  
+    expect(response.text).toMatch('The Shawshank Redemption');
+  });
+
+test('The Forrest Gump  page shows title of movie', async () => {
+    const response = await request(app).get('/movies/6')
+      .expect('Content-Type', 'text/html; charset=utf-8')
+      .expect(200);
+  
+    expect(response.text).toMatch('Forrest Gump');
+  });
+
+test('The Muppets page shows title of movie', async () => {
+    const response = await request(app).get('/movies/5')
+      .expect('Content-Type', 'text/html; charset=utf-8')
+      .expect(200);
+  
+    expect(response.text).toMatch('The Muppets');
+  });
